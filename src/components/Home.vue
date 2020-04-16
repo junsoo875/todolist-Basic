bf7913244cd368fb7dd5801b46e5e098beac8aca
junsoo <junsoo875@gmail.com>
2020-04-16 19:59:31 +0900

    first

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container>
+</v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoList: []
+    }
+  },
+  methods: {
+    listAdd(memo) {
+      console.log("받았어!")
+      this.todoList.push({ memo: memo, status: "created" })
+    }
+  }
+}
+</script>
